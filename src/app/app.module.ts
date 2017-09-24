a2afc23120ec5c341b5c92ff630ca99f4079b4a7
Aleksander Kołata <olek.tbg.konta@gmail.com>
2017-09-24 20:07:19 +0200

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    SearchBarComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule
